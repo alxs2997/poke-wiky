645d9b04fd9cb8bcfdc181850b6192fc509b54cd
galle997 <rudolfmetal7@gmail.com>
2022-02-24 23:31:08 -0600

    iniciando proyecto pokewiki

---
+++ src/components/MostrarPokemon.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+  <b-card
+    title="Card Title"
+    img-src="/img/estrella.png"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'MostrarPokemon',
+    data(){
+        return{
+            img:"@/assets/pokewiky.png"
+        }
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Home">
+    <img alt="pokewiky" src="../assets/pokewiky.png">
+    <div class="center">
+      <input type="text" v-model="idPokemon">
+      <button type="button" @click="buscar">Buscar</button>
+      <img :src="imagen" alt="">
+      <MostrarPokemon/>
+   
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import MostrarPokemon from "@/components/MostrarPokemon.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+export default{
+  components:{
+    MostrarPokemon
+  },
+  data(){
+    return{
+      show: false,
+      isActive: true,
+      imagen: "",
+      idPokemon:""
+    };
+  },
+  methods:{
+    buscar(){
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${this.idPokemon}`)
+        .then((response) =>{
+        console.log(response.data.sprites.front_default);
+        this.imagen=response.data.sprites.front_default;
+        //console.log(response);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+    }
   }
 }
 </script>
+
+<style scoped>
+img{
+    width:35%;
+    height:35%;
+  }
+</style>
